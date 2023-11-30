0b75ba04ce0eb322ef180b88b519d94b1d0e71e4
umarfchy <umarfchy@gmail.com>
2023-11-30 21:02:15 +0600

    add copy btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { CopyButton } from "~/components/CopyButton";
+
 export default function Home() {
   return (
     <main>
       <h3>Hello Personapal</h3>
+      <CopyButton value="hej" />
     </main>
   );
 }
